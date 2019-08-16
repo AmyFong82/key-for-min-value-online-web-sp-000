632d2fa77e0587b1b729cb6e2b2fc38694b94c6c
Amy Fong <amyhfong@gmail.com>
2019-08-16 17:26:28 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -12,6 +12,6 @@ def key_for_min_value(name_hash)
   }
 end
 
-ikea = {:chair => 25, :table => 85, :mattress => 450}
+ikea = {:chair => 95, :table => 85, :mattress => 450}
 
 key_for_min_value(ikea)
