8edb49c57274a26e1b5eeab4b8d8e69a514a9cd4
Amy Fong <amyhfong@gmail.com>
2019-08-16 18:13:47 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  small_key = ""
   name_hash.collect {|key, value|
     small_key = key
     small_val = value
