c51d685232ffc8714866908200484f7e28d20e0a
Amy Fong <amyhfong@gmail.com>
2019-08-16 18:07:20 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,9 +3,13 @@
 
 def key_for_min_value(name_hash)
   name_hash.each {|key, value|
-    keys = key
-    vals = value
-
+    low_key = key
+    low_val = value
+    if value < low_val
+      low_val = value
+      low_key = key
+    end
+    return low_key
   }
 end
 
